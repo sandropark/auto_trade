0ce715ecee45d5fac28458ca92cb34738a1d5ad3
sandropark <ticoba@naver.com>
2024-05-03 16:13:09 +0900

    add api logging

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from crypto import account
 from crypto.crypto_app import Crypto
 from crypto.strategy import AMStrategy, VBStrategy
+from infrastructure import chat_client
 
 app = Flask(__name__)
 crypto = Crypto([AMStrategy(), VBStrategy()])
@@ -30,12 +31,8 @@ def restart():
 @app.route('/raw-data', methods=['PUT'])
 def update_raw_data():
     crypto.__refresh__()
+    chat_client.send_message("데이터 업데이트 완료!")
     return "데이터 업데이트 완료!"
-
-@app.route('/account', methods=['PUT'])
-def update_account():
-    account.refresh()
-    return "계좌 정보 업데이트 완료!"
 
 if __name__ == '__main__':
     app.run(debug=True)
